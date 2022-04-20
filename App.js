dfce7e976c6f1f39fadfee0f6bce011c615577e4
Matt Coles <macols@amazon.com>
2022-04-20 16:48:02 +1000

    Chapter 2 End

--- App.js
+++ App.js
@@ -1,18 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Button, TextInput } from 'react-native';
+import React from 'react';
+import { Auth } from 'aws-amplify'
 import Amplify from 'aws-amplify';
 import config from './src/aws-exports';
+import { withAuthenticator } from 'aws-amplify-react-native';
 
 Amplify.configure(config);
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends React.Component {
+  async componentDidMount() {
+    const user = await Auth.currentAuthenticatedUser()
+    console.log('user:', user)
+  }
+  signOut = () => {
+    Auth.signOut()
+      .then(() => this.props.onStateChange('signedOut'))
+      .catch(err => console.log('err: ', err))
+  }
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.title}>Hello World</Text>
+        <Button onPress={this.signOut} title="Sign Out"/>
+      </SafeAreaView>
+    )
+  }
 }
+
+export default withAuthenticator(App, {
+  includeGreetings: true
+})
 
 const styles = StyleSheet.create({
   container: {
--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "rnamplifyc46768fa": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
